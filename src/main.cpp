2e439a41ef21224a8c815a8fd5a95afaf66fa49e
Ramid Khan <ramidzkh@gmail.com>
2021-05-31 07:26:36 +1000

    Don't force a framecount

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
-#include <string>
+#include <filesystem>
 #include "lib/thread_pool.hpp"
 #include "Image.h"
 
-void work(int i);
+void work(int i, const std::filesystem::path &frame_name, const std::filesystem::path &save_loc);
 
 int main() {
     thread_pool pool;
 
-    for (int i = 0; i < 6572; i++) { // manually change frame numbers
-        pool.submit(work, i);
+    int index = 0;
+
+    auto in = std::filesystem::path("in");
+    auto out = std::filesystem::path("out");
+
+    for (const auto &entry : std::filesystem::directory_iterator(in)) {
+        auto path = std::filesystem::relative(entry);
+        pool.submit(work, index++, path, out / relative(path, in));
     }
 
     pool.wait_for_tasks();
@@ -17,13 +23,11 @@ int main() {
     return 0;
 }
 
-void work(int i) {
+void work(int i, const std::filesystem::path &frame_name, const std::filesystem::path &save_loc) {
     std::cout << i << std::endl;
-    std::string frame_name("in/img_" + std::to_string(i) + ".png");
-    std::string save_loc("out/img_" + std::to_string(i) + ".png");
 
-    Image frame(frame_name.c_str());
+    Image frame(frame_name.string().c_str());
     Image frame_done = frame.quadifyFrame(i);
 
-    frame_done.write(save_loc.c_str());
+    frame_done.write(save_loc.string().c_str());
 }
